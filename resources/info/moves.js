eeaa82291e0301af22dfb669aadff3bf529297eb
Animon Battles <animonbattles@gmail.com>
2024-10-25 15:04:26 +0200

    Create moves.js

---
+++ resources/info/moves.js
@@ -0,0 +1,7 @@
+const moves = {
+    Grass: ["Solar Beam", "Grass Whip", "Mudshot", "Entanglement"],
+    Fire: ["Flame Thrower", "Blaze Kick", "Inferno"],
+    Water: ["Water Gun", "Aqua Tail", "Hydro Pump"]
+};
+
+export default moves;
