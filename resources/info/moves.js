9f43849bb67969d0bf1da41ed08a4c63b8063f61
Animon Battles <animonbattles@gmail.com>
2024-10-25 16:50:25 +0200

    Update moves.js

--- resources/info/moves.js
+++ resources/info/moves.js
@@ -1,3 +1,4 @@
+// To reuse, please contact .bjarnos on discord for permissions.
 const moves = {
     Grass: ["Solar Beam", "Grass Whip", "Mudshot", "Entanglement"],
     Fire: ["Flame Thrower", "Blaze Kick", "Inferno"],
