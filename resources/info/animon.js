4f0be1328a8501b9bc20e12593f2fa2ba3026337
Animon Battles <animonbattles@gmail.com>
2024-10-25 14:58:44 +0200

    Create animon.js

---
+++ resources/info/animon.js
@@ -0,0 +1,17 @@
+// baseattack is in %
+const species = [
+    { name: "Cocosip",
+     types: ["Grass"],
+     sprites: {
+       front: "https://i.postimg.cc/tgRz799z/cocosipfront.webp",
+       back: "https://i.postimg.cc/909dsMBc/cocosipback.webp",
+     },
+     basehp: 1000, basespeed: 10, baseattack: 80 },
+    { name: "Cocannon",
+     types: ["Grass"],
+     sprites: {
+       front: "https://i.postimg.cc/tgRz799z/cocosipfront.webp",
+       back: "https://i.postimg.cc/909dsMBc/cocosipback.webp",
+     },
+     basehp: 3000, basespeed: 8, baseattack: 100 },
+];
