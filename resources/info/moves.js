b06850209469736a3a36c59b6cef2d0089e9770d
Animon Battles <animonbattles@gmail.com>
2024-10-26 12:15:44 +0200

    Update moves.js

--- resources/info/moves.js
+++ resources/info/moves.js
@@ -1,8 +1,11 @@
-// To reuse, please contact .bjarnos on discord for permissions.
 const moves = {
     Grass: ["Solar Beam", "Grass Whip", "Mudshot", "Entanglement"],
-    Fire: ["Flame Thrower", "Blaze Kick", "Inferno"],
-    Water: ["Water Gun", "Aqua Tail", "Hydro Pump"]
 };
 
-export default moves;
+const data = {
+    "Solar Beam": {
+        name: "f"
+    }
+};
+
+export default { moves, data }
